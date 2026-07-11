0202933a5132bed0448cd65c5bce3bd8af6b42c0
Krunal022 <krunalvaghela723@gmail.com>
2026-07-11 19:20:15 +0530

    add two-way binding to registration form

--- React-Day-03/src/Pages/Register.jsx
+++ React-Day-03/src/Pages/Register.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [registerData, setregisterData] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(registerData);
+    setregisterData({ name: "", phone: "", email: "", password: "" });
+  };
+
   return (
     <div className="container py-5">
       <div className="row justify-content-center">
         <div className="col-md-6">
           <h1 className="fw-bold mb-4">Register</h1>
 
-          <form>
+          <form onSubmit={submitHandler}>
             {/* Full Name */}
             <div className="mb-3">
               <label className="form-label">Full Name</label>
               <input
+                onChange={(e) =>
+                  setregisterData({ ...registerData, name: e.target.value })
+                }
                 type="text"
+                name="name"
+                value={registerData.name}
                 className="form-control"
                 placeholder="Enter your full name"
               />
@@ -22,7 +35,12 @@ const Register = () => {
             <div className="mb-3">
               <label className="form-label">Phone Number</label>
               <input
+                onChange={(e) =>
+                  setregisterData({ ...registerData, phone: e.target.value })
+                }
                 type="tel"
+                name="phone"
+                value={registerData.phone}
                 className="form-control"
                 placeholder="Enter your phone number"
               />
@@ -32,7 +50,12 @@ const Register = () => {
             <div className="mb-3">
               <label className="form-label">Email address</label>
               <input
+                onChange={(e) =>
+                  setregisterData({ ...registerData, email: e.target.value })
+                }
                 type="email"
+                name="email"
+                value={registerData.email}
                 className="form-control"
                 placeholder="Enter your email"
               />
@@ -45,6 +68,11 @@ const Register = () => {
             <div className="mb-3">
               <label className="form-label">Password</label>
               <input
+                onChange={(e) =>
+                  setregisterData({ ...registerData, password: e.target.value })
+                }
+                name="password"
+                value={registerData.password}
                 type="password"
                 className="form-control"
                 placeholder="Enter password"
